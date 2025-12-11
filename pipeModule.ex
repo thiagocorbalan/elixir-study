c4699c6ea03fe7129aec1fd34d66e12d705fc21b
Thiago Corbalan <contato.twebdesigner@gmail.com>
2025-12-11 20:54:58 -0300

    feat: implement pipe operator

--- pipeModule.ex
+++ pipeModule.ex
@@ -1,10 +1,17 @@
 defmodule PipeModule do
-  def sum(a, b), do: a + b
-  def subtract(a, b), do: a - b
-  def multiply(a, b), do: a * b
-  def divide(a, b), do: div(a,b)
+  defp sum(a, b), do: a + b
+  defp subtract(a, b), do: a - b
+  defp multiply(a, b), do: a * b
+  defp divide(a, b), do: div(a,b)
 
   def exec(a, b) do
-    sum(multiply(divide(subtract(a,b),b), b), b)
+    # sum(multiply(divide(subtract(a,b),b), b), b)
+
+    ## Operador |> (pipe) é a formado pela "|" e ">"
+    a
+      |> subtract(b)
+      |> divide(b)
+      |> multiply(b)
+      |> sum(b)
   end
 end
