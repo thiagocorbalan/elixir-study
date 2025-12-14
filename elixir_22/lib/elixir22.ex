a8104ac1766f7b0ab249f6e703217d5ad536d589
Thiago Corbalan <contato.twebdesigner@gmail.com>
2025-12-14 15:49:43 -0300

    feat: working with script and file handler

--- elixir_22/lib/elixir22.ex
+++ elixir_22/lib/elixir22.ex
@@ -1,12 +1,52 @@
 defmodule Elixir22 do
   def main(args \\ []) do
-    { _opts, value } =
+    { opts, value } =
       args
       |> parser_args()
       |> IO.inspect()
 
+      output = opts[:output] || "busca"
+
+      # encoded = URI.encode_query(%{q: value});
+      encoded = URI.encode(value)
+      path = "https://www.elo7.com.br/categoria/casa/#{encoded}";
+
+      IO.puts "O valor de entrada é \"#{value}\" salvando em #{output}.txt"
+
+      headers = [
+        { "Accept", "*/*" },
+        {"user-agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36"}
+      ]
+
+      HTTPoison.get(path, headers, follow_redirect: true)
+        |> case do
+          {:ok, %{body: html}} ->
+            # IO.inspect(html)
+            {:ok, doc} = Floki.parse_document(html)
+
+            data = doc
+              |> Floki.find("li.product")
+              |> Enum.with_index(1)
+              |> Enum.map(fn {row, index} ->
+                title = Floki.find(row, "span.link-product")
+                  |> Floki.text()
+                "#{index} - #{title}"
+              end)
+              #|> Enum.count()
+              |> Enum.join("\n")
+              # |> IO.inspect
+
+              path = File.cwd!<>"/#{output}.txt"
+              File.write(path, data, [:write, { :encoding, :latin1}]);
+
+            IO.puts "Deu certo, #{path}"
+          {:error, _} -> IO.puts "Erro na busca"
+        end
+
+
+
     ## IO.inspect args
-    IO.puts "O valor de entrada: #{value}"
+    ## IO.puts "O valor de entrada: #{value}"
   end
 
   def parser_args(args) do
--- elixir_22/mix.exs
+++ elixir_22/mix.exs
@@ -26,6 +26,8 @@ defmodule Elixir22.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:httpoison, "~> 2.0"},
+      {:floki, "~> 0.38"},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
